f7a8fc676b56f3e38faaadffbe07c8ed0e25e74f
Alex-Lee-Myers <alexander.l.myers@gmail.com>
2021-09-24 00:43:28 -0400

    final update without stretch goals

--- apps.js
+++ apps.js
@@ -1,59 +1,70 @@
 //GLOBAL VARIABLES /* random update for august 21, 2021 class */
 // API URL
-const charURL = 'https://ghibliapi.herokuapp.com/people';
 const filmURL = 'https://ghibliapi.herokuapp.com/films';
-const locURL = 'https://ghibliapi.herokuapp.com/locations';
 
 // Call and use pre-existing HTML elements (main/section)
-let SGSreplace = document.querySelector("#SGSearch");
-let charID = document.querySelector("#charSearch");
-let filmID = document.querySelector("#filmSearch");
-let locID = document.querySelector("#locSearch");
+let filmID = document.querySelector("#newFilm");
+const filmInfo = document.getElementById('#filmInfo');
+const bcInfo = document.getElementsByClassName('.bcInfo');
 
-// -------- separator ------- Local data from JS file displayed -------
-
-// Make a display function to do all that:
-// Creating elements, Give them data, Append to HTML/site page
-//function displayFilms() {
-    //Console.logging to check if object/data is passed
-        //console.log(films);//to make sure when this gets called, is it actually receiving everything from myBio?
-
-    // Create my elements for bio info
-
-    //Give element bio data
-        //nameTag.innerText = `Hello there, my name is ${bio.name}!`;
-
-    //Append those elements to the main bio div
-        //bioDiv.appendChild(nameTag);
-
-// -------- separator ------- External data from API display -------
 // Creating the fetch to grab data
-
-fetch(charURL)
+let newFilm = () => {
+fetch(filmURL)
     .then(res => res.json())
-    .then(data => charDisplay(data))
+    .then(data => filmDisplay(data))
     .catch(err => console.log(err))
-
-let charDisplay = (data) => {
-     console.log(data);
-
-    let tempAge = data[0].age;
-    console.log(tempAge);
 }
-//       console.log(data);
 
+let filmDisplay = (data) => {
+    while(bcInfo.firstChild) {
+        bcInfo.removeChild(bcInfo.firstChild);
+    }
 
-    // create new elements for incoming data
-       //let img1 = document.createElement("img"); //img = thats the tag.
+    while(buttonClick.firstChild) { //if it has a first child
+        buttonClick.removeChild(buttonClick.firstChild); //then remove that child
+    }
 
-    // Give new elements attributes and data to display 
-       //img1.className = "cards"; //could do this with a loop
+    //random array values
+    let filmArray = [data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7], data[8], data[9], data[10], data[11], data[12], data[13], data[14], data[15], data[16], data[17], data[18], data[19], data[20]];
+    let theArray = filmArray[Math.floor(Math.random() * filmArray.length)];
+    // console.log(theArray);
 
-    // Populating card/img1's attributes
-        //img1.src = `${card[0].image}`;
-        //img1.alt = `A playing card that is the ${card[0].value} of ${card[0].suit.toLowerCase()}`;
+    //Create elements for random array pull
+    let titleOfFilm = document.createElement('h1');
+    let jpnTitle = document.createElement('h3');
+    let directorOfFilm = document.createElement('h3');
+    let descriptionOfFilm = document.createElement('p');
+    let rtScore = document.createElement('h3');
+    let releaseDate = document.createElement('h3');
+    
 
+    //give data
 
+    titleOfFilm.innerText = `${theArray.title}`;   
+    jpnTitle.innerText = `${theArray.original_title}`;
+    directorOfFilm.innerText = `Director: ${theArray.director}`; //stretch goal is italizing the calling in element
+    descriptionOfFilm.innerText = `${theArray.description}`;
+    rtScore.innerText = `RottenTomatoe's Score: ${theArray.rt_score}`; //stretch goal is italizing the calling in element
+    releaseDate.innerText = `Release Year: ${theArray.release_date}`; 
+    
+    
+    // STRETCH GOAL
+    // 1.) remove the main image on click
+    // 2.) replace buttonClick info on top of it
+        // 2a.) smooth transition of this
+    // 3.) SGSreplace main image returns after 20 seconds OR reset button hit
+    let SGSreplace = document.querySelector("#SGSearch");
+    // SGSreplace.remove();
+    // while(SGSreplace = './assets/StudioGhibliSearch.png') { //if it has a first child
+    //     SGSreplace.remove; //then remove that child
+    // }
 
-    //Appending the cards/images to the HTML; through apiDiv
-        //apiDiv.appendChild(img1);
+    //append the above to HTML to display
+    buttonClick.appendChild(titleOfFilm);
+    buttonClick.appendChild(jpnTitle);
+    buttonClick.appendChild(directorOfFilm);
+    buttonClick.appendChild(descriptionOfFilm);
+    buttonClick.appendChild(rtScore);
+    buttonClick.appendChild(releaseDate);
+    
+}
